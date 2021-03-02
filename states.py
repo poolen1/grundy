fff3b94dead0f90039fe8bd011f43c919adedf7d
poolen108 <poolen108@gmail.com>
2021-03-02 12:11:32 -0800

    welp lol

--- states.py
+++ states.py
@@ -18,7 +18,8 @@ class TreeNode:
     def update_label(self, label):
         self.label = label
 
-class TraverseTree:
+
+class GameTree:
     def __init__(self, start_state):
         self.start_node = TreeNode(start_state)
         self.open_nodes = deque()
@@ -29,9 +30,6 @@ class TraverseTree:
         while self.open_nodes:
             n = self.open_nodes.pop()
             self.closed_nodes.appendleft(n)
-
-            # print("state: ", n.state.open + n.state.closed)
-            # print("open nodes: ", self.open_nodes)
 
             for stack in n.state.open:
                 low_range = int(stack / 2) + 1
@@ -55,4 +53,30 @@ class TraverseTree:
 
                     self.open_nodes.appendleft(new_node)
 
-        return self.closed_nodes
+        closed_nodes = self.calculate_state_costs(list(self.closed_nodes))
+
+        return closed_nodes
+
+    # sum cost
+    @staticmethod
+    def calculate_state_costs(tree):
+        for node in tree:
+            if not node.successors:
+                parent = node.parent
+                while parent:
+                    branch = False
+
+                    parent.cost += node.cost + 1
+
+                    if len(parent.successors) > 1:
+                        for i in parent.successors:
+                            if i.cost == 0:
+                                parent = None
+                                branch = True
+                    if branch is True:
+                        continue
+
+                    node = parent
+                    parent = parent.parent
+
+        return tree
